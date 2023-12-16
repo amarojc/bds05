f8f9fe9b7885d86b43b8c0f17180bf1b1baaaee6
Jorge Amaro <inf.amaro.jc@gmail.com>
2023-12-15 22:44:32 -0300

    Fix MovieRepository

--- src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/MovieRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.movieflix.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.movieflix.entities.Genre;
 import com.devsuperior.movieflix.entities.Movie;
 
 public interface MovieRepository extends JpaRepository<Movie, Long>{
 
+	@Query("SELECT movie FROM Movie movie "
+			+ "WHERE movie.genre IN :genres "
+			+ "ORDER BY movie.title ASC")
+	Page<Movie> findByGenre(List<Genre> genres, Pageable pageable);
+	
 }
